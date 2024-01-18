40f799c7338e25788eeadb5f63cdc9dd21d5b899
Daniel Muiruri <gathigidg26@gmail.com>
2024-01-18 10:16:58 +0300

    :alembic: Designing my pf

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+import fashions from '../assets/original.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"Fashions App",
+        image:fashions,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:fashions,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Angular Application",
+        image:fashions,
+        github:"",
+        live:"",
+    },
+
+
+
+]
