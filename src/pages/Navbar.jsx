08cb187bc1c2cd51567ee474782960ad6c7688bc
Daniel Muiruri <gathigidg26@gmail.com>
2024-01-30 22:13:55 +0300

    :rocket: Nearly ready Pf

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className=' lg:flex fixed flex-col top-[6%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a className='flex justify-between items-center w-full text-gray-300' href='https://www.linkedin.com/in/daniel-muiruri-541a701a3/'>
