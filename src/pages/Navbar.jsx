0933a9c49a72f1489ba8667ab5099c6590cf6f74
Daniel Muiruri <gathigidg26@gmail.com>
2024-02-19 16:06:46 +0300

    Update Navbar.jsx - Resume part

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -111,7 +111,7 @@ const Navbar = () => {
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a className='flex justify-between items-center w-full text-gray-300' href='https://drive.google.com/file/d/1eSsU6ormC49l2H1Tie_VGvVebZnsLR0x/view?usp=sharing'>
+            <a className='flex justify-between items-center w-full text-gray-300' href='https://drive.google.com/file/d/1uBEpMS2gJMLD6nfUBRWbSe2kXzNuVI3x/view?usp=sharing'>
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
