373d4d7b6afd90ed25fcb02648df85a435db2e0f
Daniel Muiruri <gathigidg26@gmail.com>
2024-04-10 21:42:58 +0300

    :deploy:Added A new project that i was working on - it is purely frontend using react

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,31 @@
-import fashions from '../assets/original.jpg'
-import rf from '../assets/react.jpg'
+import fashions from '../assets/original.jpg';
+import rf from '../assets/react.jpg';
+import rs from '../assets/nyumbaa.png'
 
 export const data=[
     {
         id:1,
-        name:"Fashions App",
-        paragraph:"FastAPI + React",
-        image:fashions,
+        name:"Real-Estate-App",
+        paragraph:"React + Tailwindcss",
+        image:rs,
         github:"",
-        live:"",
+        live:"https://gethomes.netlify.app/",
     },
     {
         id:2,
         name:"React JS Application",
-        paragraph:"React",
+        paragraph:"Fetching data from db.json file",
         image:rf,
         github:"",
         live:"https://pet-kingdom-project.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Fashions App",
+        paragraph:"Still in progress",
+        image:fashions,
+        github:"",
+        live:"https://github.com/danielsdgg/fashions.git",
     },
 
 
