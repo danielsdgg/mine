ad0d90d14680e95c096190b4ce8a321b1554c9e2
Daniel Muiruri <gathigidg26@gmail.com>
2026-02-03 20:03:32 +0300

    updates on data file - projects

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import fashions from '../assets/original.jpg';
 import rs from '../assets/crest.jpg'
+import insurance from '../assets/nyumbaa.png';
 
 export const data=[
     {
@@ -11,12 +12,20 @@ export const data=[
         live:"https://morgantechnicaltraining.com/",
     },
     {
+        id:2,
+        name:"Insurance Website",
+        paragraph:"Created using React + Java(springboot)",
+        image:insurance,
+        github:"",
+        live:"https://insuranceke.netlify.app/",
+    },
+    {
         id:3,
-        name:"Alpha-Homes",
-        paragraph:"Created using Flask + React",
+        name:"Fashions Design",
+        paragraph:"Created using React + Python(Flask)",
         image:fashions,
         github:"",
-        live:"https://final-projects-huib.onrender.com/",
+        live:"https://heroclothline.netlify.app/",
     },
 
 
